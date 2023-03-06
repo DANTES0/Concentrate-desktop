307fe5a204e5b5c967954e5a0b1531e9288efe87
Bondike <116958466+Bond1ke@users.noreply.github.com>
2023-03-06 23:18:34 +0300

    basic navigation through pages

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import sys
 from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
+from PyQt5.uic import loadUi
+class MainUI(QMainWindow):
+    def __init__(self):
+        super(MainUI, self).__init__()
+        loadUi("bidgo.ui", self)
 
-#new code from bogdan
+        # PAGE 1
+        self.btn_page_1.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_1))
+
+        # PAGE 2
+        self.btn_page_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_2))
+
+        # PAGE 3
+        self.btn_page_3.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_3))
+
+        # PAGE 4
+        self.btn_page_4.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_4))
 class MyGui(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
@@ -25,10 +41,12 @@ class MyGui(QtWidgets.QWidget):
         #        print(self.time.toString("hh:mm"))
         self.lcdTime.display(self.time.toString("hh:mm:ss"))
 
-
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    w = MyGui()
-    w.resize(300, 120)
-    w.show()
+    app = QApplication(sys.argv)
+    ui = MainUI()
+    ui.show()
+    #MainWindow.show()
+    #w = MyGui()
+    #w.resize(300, 120)
+    #w.show()
     sys.exit(app.exec_())
