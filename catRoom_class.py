4f1e3dd6f0f1445dc1f4d8512f3e69b8837d4c3d
Bondike <116958466+Bond1ke@users.noreply.github.com>
2023-03-29 19:18:41 +0300

    магазин с котиками без кнопок

--- catRoom_class.py
+++ catRoom_class.py
@@ -47,6 +47,22 @@ class CatRoom(QMainWindow):
         self.mywidget.shadow_6.close()
         self.mywidget.shadow_7.close()
         self.mywidget.shadow_8.close()
+        self.mywidget.cat_1.close()
+        self.mywidget.cat_2.close()
+        self.mywidget.cat_3.close()
+        self.mywidget.cat_4.close()
+        self.mywidget.cat_5.close()
+        self.mywidget.cat_6.close()
+        self.mywidget.cat_7.close()
+        self.mywidget.cat_8.close()
+        self.mywidget.cat_name_1.close()
+        self.mywidget.cat_name_2.close()
+        self.mywidget.cat_name_3.close()
+        self.mywidget.cat_name_4.close()
+        self.mywidget.cat_name_5.close()
+        self.mywidget.cat_name_6.close()
+        self.mywidget.cat_name_7.close()
+        self.mywidget.cat_name_8.close()
         self.storeButton_load.show()
         self.mywidget.storeButton_exit.hide()
     def init_Ui(self):
@@ -93,10 +109,25 @@ class CatRoom(QMainWindow):
         self.mywidget.shadow_1.show()
 
         #  кнопка 1
-        self.mywidget.button_1 = QPushButton(self)
+        self.mywidget.button_1 = QLabel(self)
         self.mywidget.button_1.setGeometry(113, 96, 146, 180)
         self.mywidget.button_1.setStyleSheet("background:#FFFFFF; border-radius: 30px;")
         self.mywidget.button_1.show()
+
+        #  котик для кнопки 1
+        self.mywidget.cat_1 = QLabel(self)
+        pixmap = QPixmap('source/Sleeper.png')
+        self.mywidget.cat_1.setPixmap(pixmap)
+        self.mywidget.cat_1.setGeometry(133, 133, 106, 106)
+        self.mywidget.cat_1.setStyleSheet("background:transparent")
+        self.mywidget.cat_1.show()
+
+        #  текст для кнопки 1
+        self.mywidget.cat_name_1 = QLabel(self)
+        self.mywidget.cat_name_1.setGeometry(137, 105, 100, 40)
+        self.mywidget.cat_name_1.setText("Sleeper")
+        self.mywidget.cat_name_1.setStyleSheet("background: transparent; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 23px; line-height: 15px; text-align: center;")
+        self.mywidget.cat_name_1.show()
 
         #  тень кнопки 2
         self.mywidget.shadow_2 = QLabel()
@@ -106,10 +137,25 @@ class CatRoom(QMainWindow):
         self.mywidget.shadow_2.show()
 
         #  кнопка 2
-        self.mywidget.button_2 = QPushButton(self)
+        self.mywidget.button_2 = QLabel(self)
         self.mywidget.button_2.setGeometry(277, 96, 146, 180)
         self.mywidget.button_2.setStyleSheet("background:#FFFFFF; border-radius: 30px;")
         self.mywidget.button_2.show()
+
+        #  котик для кнопки 2
+        self.mywidget.cat_2 = QLabel(self)
+        pixmap = QPixmap('source/Jokey.png')
+        self.mywidget.cat_2.setPixmap(pixmap)
+        self.mywidget.cat_2.setGeometry(283, 110, 138, 131)
+        self.mywidget.cat_2.setStyleSheet("background:transparent")
+        self.mywidget.cat_2.show()
+
+        #  текст для кнопки 2
+        self.mywidget.cat_name_2 = QLabel(self)
+        self.mywidget.cat_name_2.setGeometry(316, 105, 100, 40)
+        self.mywidget.cat_name_2.setText("Jokey")
+        self.mywidget.cat_name_2.setStyleSheet("background: transparent; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 23px; line-height: 15px; text-align: center;")
+        self.mywidget.cat_name_2.show()
 
         #  тень кнопки 3
         self.mywidget.shadow_3 = QLabel()
@@ -119,72 +165,176 @@ class CatRoom(QMainWindow):
         self.mywidget.shadow_3.show()
 
         #  кнопка 3
-        self.mywidget.button_3 = QPushButton(self)
+        self.mywidget.button_3 = QLabel(self)
         self.mywidget.button_3.setGeometry(441, 96, 146, 180)
         self.mywidget.button_3.setStyleSheet("background:#FFFFFF; border-radius: 30px;")
         self.mywidget.button_3.show()
+
+        #  котик для кнопки 3
+        self.mywidget.cat_3 = QLabel(self)
+        pixmap = QPixmap('source/Frisky.png')
+        self.mywidget.cat_3.setPixmap(pixmap)
+        self.mywidget.cat_3.setGeometry(455, 145, 133, 83)
+        self.mywidget.cat_3.setStyleSheet("background:transparent")
+        self.mywidget.cat_3.show()
+
+        #  текст для кнопки 3
+        self.mywidget.cat_name_3 = QLabel(self)
+        self.mywidget.cat_name_3.setGeometry(481, 105, 100, 40)
+        self.mywidget.cat_name_3.setText("Frisky")
+        self.mywidget.cat_name_3.setStyleSheet("background: transparent; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 23px; line-height: 15px; text-align: center;")
+        self.mywidget.cat_name_3.show()
 
         #  тень кнопки 4
         self.mywidget.shadow_4 = QLabel()
         self.mywidget.shadow_4.setGeometry(116, 311, 152, 180)
         self.mywidget.shadow_4.setParent(self)
-        self.mywidget.shadow_4.setStyleSheet("background:#455D81; border-radius: 30px;")
+        self.mywidget.shadow_4.setStyleSheet("background:#613795; border-radius: 30px;")
         self.mywidget.shadow_4.show()
 
         #  кнопка 4
-        self.mywidget.button_4 = QPushButton(self)
+        self.mywidget.button_4 = QLabel(self)
         self.mywidget.button_4.setGeometry(113, 304, 146, 180)
         self.mywidget.button_4.setStyleSheet("background:#FFFFFF; border-radius: 30px;")
         self.mywidget.button_4.show()
+
+        #  котик для кнопки 4
+        self.mywidget.cat_4 = QLabel(self)
+        pixmap = QPixmap('source/Dreamer.png')
+        self.mywidget.cat_4.setPixmap(pixmap)
+        self.mywidget.cat_4.setGeometry(115, 330, 139, 124)
+        self.mywidget.cat_4.setStyleSheet("background:transparent")
+        self.mywidget.cat_4.show()
+
+        #  текст для кнопки 4
+        self.mywidget.cat_name_4 = QLabel(self)
+        self.mywidget.cat_name_4.setGeometry(136, 310, 100, 40)
+        self.mywidget.cat_name_4.setText("Dreamer")
+        self.mywidget.cat_name_4.setStyleSheet("background: transparent; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 23px; line-height: 15px; text-align: center;")
+        self.mywidget.cat_name_4.show()
 
         #  тень кнопки 5
         self.mywidget.shadow_5 = QLabel()
         self.mywidget.shadow_5.setGeometry(280, 311, 152, 180)
         self.mywidget.shadow_5.setParent(self)
-        self.mywidget.shadow_5.setStyleSheet("background:#814545; border-radius: 30px;")
+        self.mywidget.shadow_5.setStyleSheet("background:#613795; border-radius: 30px;")
         self.mywidget.shadow_5.show()
 
         # кнопка 5
-        self.mywidget.button_5 = QPushButton(self)
+        self.mywidget.button_5 = QLabel(self)
         self.mywidget.button_5.setGeometry(277, 304, 146, 180)
         self.mywidget.button_5.setStyleSheet("background:#FFFFFF; border-radius: 30px;")
         self.mywidget.button_5.show()
+
+        #  котик для кнопки 5
+        self.mywidget.cat_5 = QLabel(self)
+        pixmap = QPixmap('source/Fluffy.png')
+        self.mywidget.cat_5.setPixmap(pixmap)
+        self.mywidget.cat_5.setGeometry(300, 354, 97, 72)
+        self.mywidget.cat_5.setStyleSheet("background:transparent")
+        self.mywidget.cat_5.show()
+
+        #  текст для кнопки 5
+        self.mywidget.cat_name_5 = QLabel(self)
+        self.mywidget.cat_name_5.setGeometry(318, 310, 100, 40)
+        self.mywidget.cat_name_5.setText("Fluffy")
+        self.mywidget.cat_name_5.setStyleSheet("background: transparent; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 23px; line-height: 15px; text-align: center;")
+        self.mywidget.cat_name_5.show()
 
         #  тень кнопки 6
         self.mywidget.shadow_6 = QLabel()
         self.mywidget.shadow_6.setGeometry(444, 311, 152, 180)
         self.mywidget.shadow_6.setParent(self)
-        self.mywidget.shadow_6.setStyleSheet("background:#814545; border-radius: 30px;")
+        self.mywidget.shadow_6.setStyleSheet("background:#613795; border-radius: 30px;")
         self.mywidget.shadow_6.show()
 
         #  кнопка 6
-        self.mywidget.button_6 = QPushButton(self)
+        self.mywidget.button_6 = QLabel(self)
         self.mywidget.button_6.setGeometry(441, 304, 146, 180)
         self.mywidget.button_6.setStyleSheet("background:#FFFFFF; border-radius: 30px;")
         self.mywidget.button_6.show()
+
+        #  котик для кнопки 6
+        self.mywidget.cat_6 = QLabel(self)
+        pixmap = QPixmap('source/Mr_Chief.png')
+        self.mywidget.cat_6.setPixmap(pixmap)
+        self.mywidget.cat_6.setGeometry(468, 350, 96, 79)
+        self.mywidget.cat_6.setStyleSheet("background:transparent")
+        self.mywidget.cat_6.show()
+
+        #  текст для кнопки 6
+        self.mywidget.cat_name_6 = QLabel(self)
+        self.mywidget.cat_name_6.setGeometry(464, 310, 100, 40)
+        self.mywidget.cat_name_6.setText("Mr.Chief")
+        self.mywidget.cat_name_6.setStyleSheet("background: transparent; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 23px; line-height: 15px; text-align: center;")
+        self.mywidget.cat_name_6.show()
 
         #  тень кнопки 7
         self.mywidget.shadow_7 = QLabel()
         self.mywidget.shadow_7.setGeometry(193, 521, 152, 180)
         self.mywidget.shadow_7.setParent(self)
-        self.mywidget.shadow_7.setStyleSheet("background:#814545; border-radius: 30px;")
+        self.mywidget.shadow_7.setStyleSheet("background:#ad1717; border-radius: 30px;")
         self.mywidget.shadow_7.show()
 
         #  кнопка 7
-        self.mywidget.button_7 = QPushButton(self)
+        self.mywidget.button_7 = QLabel(self)
         self.mywidget.button_7.setGeometry(190, 512, 146, 180)
         self.mywidget.button_7.setStyleSheet("background:#FFFFFF; border-radius: 30px;")
         self.mywidget.button_7.show()
+
+        #  котик для кнопки 7
+        self.mywidget.cat_7 = QLabel(self)
+        pixmap = QPixmap('source/Prince.png')
+        self.mywidget.cat_7.setPixmap(pixmap)
+        self.mywidget.cat_7.setGeometry(220, 560, 90, 90)
+        self.mywidget.cat_7.setStyleSheet("background:transparent")
+        self.mywidget.cat_7.show()
+
+        #  текст для кнопки 7
+        self.mywidget.cat_name_7 = QLabel(self)
+        self.mywidget.cat_name_7.setGeometry(228, 515, 100, 40)
+        self.mywidget.cat_name_7.setText("Prince")
+        self.mywidget.cat_name_7.setStyleSheet("background: transparent; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 23px; line-height: 15px; text-align: center;")
+        self.mywidget.cat_name_7.show()
 
         #  тень кнопки 8
         self.mywidget.shadow_8 = QLabel()
         self.mywidget.shadow_8.setGeometry(357, 521, 152, 180)
         self.mywidget.shadow_8.setParent(self)
-        self.mywidget.shadow_8.setStyleSheet("background:#814545; border-radius: 30px;")
+        self.mywidget.shadow_8.setStyleSheet("background:#ad1717; border-radius: 30px;")
         self.mywidget.shadow_8.show()
 
         #  кнопка 8
-        self.mywidget.button_8 = QPushButton(self)
+        self.mywidget.button_8 = QLabel(self)
         self.mywidget.button_8.setGeometry(354, 512, 146, 180)
         self.mywidget.button_8.setStyleSheet("background:#FFFFFF; border-radius: 30px")
         self.mywidget.button_8.show()
+
+        #  котик для кнопки 8
+        self.mywidget.cat_8 = QLabel(self)
+        pixmap = QPixmap('source/Kirill.png')
+        self.mywidget.cat_8.setPixmap(pixmap)
+        self.mywidget.cat_8.setGeometry(365, 560, 128, 80)
+        self.mywidget.cat_8.setStyleSheet("background:transparent")
+        self.mywidget.cat_8.show()
+
+        #  текст для кнопки 8
+        self.mywidget.cat_name_8 = QLabel(self)
+        self.mywidget.cat_name_8.setGeometry(402, 515, 100, 40)
+        self.mywidget.cat_name_8.setText("Kirill")
+        self.mywidget.cat_name_8.setStyleSheet("background: transparent; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 23px; line-height: 15px; text-align: center;")
+        self.mywidget.cat_name_8.show()
+
+        #  пояснение 1
+        # self.mywidget.cats_group_1 = QLabel(self)
+        # self.mywidget.cats_group_1.setGeometry(91, 545, 93, 56)
+        # self.mywidget.cats_group_1.setText("regular\n   cats")
+        # self.mywidget.cats_group_1.setStyleSheet("background: #D9D9D9; border: 2.5px dashed #455D81; border-radius: 25px; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 20px; line-height: 15px; text-align: center;")
+        # self.mywidget.cats_group_1.show()
+
+        #  пояснение 2
+        # self.mywidget.cats_group_1 = QLabel(self)
+        # self.mywidget.cats_group_1.setGeometry(91, 620, 93, 56)
+        # self.mywidget.cats_group_1.setText("   rare\n   cats")
+        # self.mywidget.cats_group_1.setStyleSheet("background: #D9D9D9; border: 2.5px dashed #814545; border-radius: 25px; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 20px; line-height: 15px; text-align: center;")
+        # self.mywidget.cats_group_1.show()
