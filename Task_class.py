e4cd96b62b75f58ca97e60cfd404c46fdde28078
Hjjik <vaaa@mail.ru>
2023-03-30 22:53:15 +0300

    Сделан бэк статистики

--- Task_class.py
+++ Task_class.py
@@ -10,6 +10,9 @@ class Task(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.height = 314
+        self.line = 0
+        self.prevSender = None
         self.title = "Meow concentration"
         self.scene = QGraphicsScene()
         self.view = QGraphicsView(self.scene)
@@ -18,6 +21,7 @@ class Task(QMainWindow):
         self.add_task()
         self.cat()
         self.TaskList()
+        # self.TaskAdd(self.height)
         self.InitWindow()
     def InitWindow(self):
         self.setWindowTitle(self.title)
@@ -35,6 +39,11 @@ class Task(QMainWindow):
         pixmap = QPixmap('source/plus.png')
         Icon = QIcon(pixmap)
         self.labelPlus.setIcon(Icon)
+        self.labelPlus.clicked.connect(self.action)
+
+    def action(self):
+        self.height = self.height+57
+        self.TaskAdd(self.height)
 
 
     def cat(self):
@@ -47,3 +56,64 @@ class Task(QMainWindow):
         self.labelTaskList = QLabel("  Task list", self)
         self.labelTaskList.setGeometry(51, 314, 127, 50)
         self.labelTaskList.setStyleSheet("background: #8350AA; border-radius: 25px; font-family:Inter; font:24px; font-weight:bold; color:#ffffff;  ")
+
+    def TaskAdd(self, height):
+        height = self.height
+
+        self.taskListHell = QLabel(self)
+        self.taskListHell.setGeometry(51, height, 598, 50)
+        self.taskListHell.setStyleSheet("background:#ffffff; border-radius:24px;")
+        self.taskListHell.setObjectName(f'{self.line}')
+        self.line += 1
+        self.taskListHell.setText(self.labelTask.text())
+        self.labelTask.setText('')
+        print(self.taskListHell.objectName())
+
+        self.circle = QLabel(self)
+        self.circle.setGeometry(69, height + 20, 9, 9)
+        self.circle.setStyleSheet("background:#8350AA; border-radius: 4px")
+        self.circle.setObjectName(f'{self.line}')
+        self.line+=1
+
+        self.acceptBtn = QPushButton(self)
+        self.acceptBtn.setGeometry(606, height + 10, 29, 29)
+        self.acceptBtn.setIcon(QIcon('source/accept.png'))
+        self.acceptBtn.setIconSize(QSize(29, 29))
+        self.acceptBtn.setStyleSheet("background:transparent")
+        self.acceptBtn.setObjectName(f'{self.line}')
+        self.line += 1
+
+        self.acceptBtn.clicked.connect(self.actionClose)
+
+        self.taskListHell.show()
+        self.circle.show()
+        self.acceptBtn.show()
+
+    def actionClose(self):
+        sender = self.sender()
+        self.prevSender = sender
+        print(str(self.sender().objectName()))
+        try:
+            if self.sender().objectName() == '2':
+                res = self.findChild(QObject, '0')
+                res.deleteLater()
+                res = self.findChild(QObject, '2')
+                res.deleteLater()
+                res = self.findChild(QObject,'1')
+                res.deleteLater()
+                res = self.findChild(QObject, '3')
+                res.setGeometry(51,371,598,50)
+                res = self.findChild(QObject, '4')
+                res.setGeometry(69,371+20, 9, 9 )
+                res = self.findChild(QObject, '5')
+                res.setGeometry(606,371+10, 29, 29)
+            # if self.sender().objectName() == '4':
+            #     res = self.findChild(QObject, '3')
+            #     res.deleteLater()
+            #     res = self.findChild(QObject, '2')
+            #     res.deleteLater()
+
+        except:
+            print('Как же хочется жить')
+
+
