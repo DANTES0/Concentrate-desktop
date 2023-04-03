46ed40811af64326b8dc11d195d785072729ed0c
Hjjik <vaaa@mail.ru>
2023-04-03 20:56:47 +0300

    Кнопочки денег и анимации

--- catRoom_class.py
+++ catRoom_class.py
@@ -48,6 +48,24 @@ class Buy_cat_button(QPushButton):
         self.xsize = 97
         self.ysize = 31
         self.price = price
+        self.cat_name = ''
+
+        if self.price == 540:
+            self.cat_name = 'Sleeper'
+        if self.price == 1080:
+            self.cat_name = 'Jokey'
+        if self.price == 2160:
+            self.cat_name = 'Frisky'
+        if self.price == 5540:
+            self.cat_name = 'Dreamer'
+        if self.price == 9540:
+            self.cat_name = 'Fluffy'
+        if self.price == 12540:
+            self.cat_name = 'Mr.Chief'
+        if self.price == 50000:
+            self.cat_name = 'Prince'
+        if self.price == 99999:
+            self.cat_name = 'Kirill'
         self.setGeometry(self.xcor, self.ycor, self.xsize, self.ysize)
         self.setIcon(QIcon("source/Coin.png"))
         self.setLayoutDirection(Qt.RightToLeft)
@@ -61,13 +79,57 @@ class Buy_cat_button(QPushButton):
         self.anim = QPropertyAnimation(self, b"geometry")
         self.anim.setEasingCurve(QEasingCurve.InOutSine)
         self.anim.setDuration(150)
+
+        self.data_base = sqlite3.connect('details.db')
+        self.cur = self.data_base.cursor()
+        self.cur.execute(f"SELECT Bought FROM Cats WHERE Cost = {self.price}")
+        self.test = self.cur.fetchone()
+        self.data_base.close()
+        # if self.test[0] == 1:
+        #     if self.price == 540:
+        #         self.setStyleSheet("background-color: #000000")
+        if self.test[0] == 1:
+            # self.setStyleSheet("background-color: #000000")
+            self.setText("Sold")
+            self.setStyleSheet(
+                "background: #D9D9D9; border: 1.5px dashed #29002F; border-radius: 15px;font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 15px; line-height: 15px; text-align: center;padding: 0px 30px 0px 5px;"
+                "color: #29002F;")
+            self.setIcon(QIcon("source"))
+            self.setEnabled(False)
+
     def aboba(self):
-            print("Я РАБОТАЮ")
+        self.data_base = sqlite3.connect('details.db')
+        self.cur = self.data_base.cursor()
+        self.cur.execute("SELECT money FROM money")
+        self.money = self.cur.fetchone()
+        self.data_base.close()
+        self.temp_price = self.money[0]
+        if self.temp_price >= self.price:
+            self.temp_price = self.temp_price - self.price
+            self.data_base = sqlite3.connect('details.db')
+            self.cur = self.data_base.cursor()
+            self.cur.execute(f"UPDATE money SET money = {self.temp_price}")
+            self.data_base.commit()
+            self.cur.execute(f"UPDATE Cats SET Bought = 1 WHERE Cost = {self.price}")
+            self.data_base.commit()
+            self.data_base.close()
+            self.setText("Sold")
+            self.setStyleSheet(
+                "background: #D9D9D9; border: 1.5px dashed #29002F; border-radius: 15px;font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 15px; line-height: 15px; text-align: center;padding: 0px 30px 0px 5px;"
+                "color: #29002F;")
+            self.setIcon(QIcon("source"))
+            self.setEnabled(False)
 
-
+        print(self.cat_name)
+        print("Я РАБОТАЮ")
 
     def enterEvent(self, event: QEvent) -> None:
-        #self.resize_obj()
+        self.data_base = sqlite3.connect('details.db')
+        self.cur = self.data_base.cursor()
+        self.cur.execute(f"SELECT Bought FROM Cats WHERE Cost = {self.price}")
+        self.test = self.cur.fetchone()
+        self.data_base.close()
+        # self.resize_obj()
         self.setGeometry(self.xcor, self.ycor, self.xsize, self.ysize)
         initial_rect = self.geometry()
         final_rect = QRect(
@@ -80,17 +142,43 @@ class Buy_cat_button(QPushButton):
         self.anim.setStartValue(initial_rect)
         self.anim.setEndValue(final_rect)
 
-        self.setText("BUY")
-        self.setStyleSheet("background: #5ee672; border: 1.5px dashed #29002F; border-radius: 15px; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 16px; line-height: 15px; text-align: center; padding: 0px 20px 0px 5px;")
-        self.anim.setDirection(QAbstractAnimation.Forward)
-        self.anim.start()
+        if self.test[0] == 1:
+            self.setText("Sold")
+            self.setStyleSheet(
+                "background: #D9D9D9; border: 1.5px dashed #29002F; border-radius: 15px;font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 15px; line-height: 15px; text-align: center;padding: 0px 30px 0px 5px;"
+                "color: #29002F; text-align:center;")
+            self.setIcon(QIcon("source"))
+            self.setEnabled(False)
+            self.anim.setDirection(QAbstractAnimation.Forward)
+            self.anim.start()
+        else:
+            self.setText("BUY")
+            self.setStyleSheet("background: #5ee672; border: 1.5px dashed #29002F; border-radius: 15px; font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 16px; line-height: 15px; text-align: center; padding: 0px 20px 0px 5px;")
+            self.anim.setDirection(QAbstractAnimation.Forward)
+            self.anim.start()
     def leaveEvent(self, event: QEvent) -> None:
-        #self.resize_obj()
+        # self.resize_obj()
+        self.data_base = sqlite3.connect('details.db')
+        self.cur = self.data_base.cursor()
+        self.cur.execute(f"SELECT Bought FROM Cats WHERE Cost = {self.price}")
+        self.test = self.cur.fetchone()
+        self.data_base.close()
         self.setGeometry(self.xcor, self.ycor, self.xsize, self.ysize)
-        self.setText(str(self.price))
-        self.setStyleSheet("background: #D8B5E9; border: 1.5px dashed #29002F; border-radius: 15px;font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 15px; line-height: 15px; text-align: center;padding: 0px 15px 0px 5px;")
-        self.anim.setDirection(QAbstractAnimation.Backward)
-        self.anim.start()
+        if self.test[0] == 1:
+            self.setText("Sold")
+            self.setStyleSheet(
+                "background: #D9D9D9; border: 1.5px dashed #29002F; border-radius: 15px;font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 15px; line-height: 15px; text-align: center;padding: 0px 30px 0px 5px;"
+                "color: #29002F; text-align:center;")
+            self.setIcon(QIcon("source"))
+            self.setEnabled(False)
+            self.anim.setDirection(QAbstractAnimation.Backward)
+            self.anim.start()
+        else:
+            self.setText(str(self.price))
+            self.setStyleSheet("background: #D8B5E9; border: 1.5px dashed #29002F; border-radius: 15px;font-family: 'Inter'; font-style: normal; font-weight: 700; font-size: 15px; line-height: 15px; text-align: center;padding: 0px 15px 0px 5px;")
+            self.anim.setDirection(QAbstractAnimation.Forward)
+            self.anim.start()
+
 class CatRoom(QMainWindow):
     def __init__(self):
         super().__init__()  # super() lets you avoid referring to the base class explicitly
@@ -104,11 +192,20 @@ class CatRoom(QMainWindow):
         self.moneyExpo = self.cur.fetchone()
         self.data_base.close()
         self.init_Ui()
+        self.UpdateCat_Room('source/Sleeper_Room.png', 30, 360, 133, 93, 540)
         self.InitWindow()
 
-    def MoneyUpdate(self):
-        self.moneyLable.setText(str(self.moneyExpo[0]))
-        print(self.moneyExpo)
+    def UpdateCat_Room(self, path, xcor, ycor, xsize, ysize, cat_name):
+        self.data_base = sqlite3.connect('details.db')
+        self.cur = self.data_base.cursor()
+        self.cur.execute(f'SELECT Bought FROM Cats WHERE Cost = {cat_name}')
+        self.test = self.cur.fetchone()
+        self.data_base.close()
+        if self.test[0] == 1:
+            self.PicLabel = QLabel(self)
+            self.PicLabel.setPixmap(QPixmap(path))
+            self.PicLabel.setGeometry(xcor, ycor, xsize, ysize)
+            self.PicLabel.setStyleSheet("background: transparent;")
     def InitWindow(self):
         self.setGeometry(650, 50, 700, 762)
         self.setFixedSize(QSize(700, 762))
